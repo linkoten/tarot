55e84e50c43c08c5146c4e12cd71a1cfbd29d4ce
linkoten <catto.francois@hotmail.fr>
2025-02-20 17:50:13 +0100

    ...

--- app/api/cartes/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { serializeDecimal } from "@/lib/utils";
-import { prisma } from "@/lib/db";
-
-export async function GET() {
-  const cartes = await prisma.carte.findMany();
-  const serializedCartes = serializeDecimal(cartes);
-  return NextResponse.json(serializedCartes);
-}
